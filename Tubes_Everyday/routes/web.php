6602f8c67f3434df20a6744079ef6d7a79cd87dd
IrhamBae <new email>
2024-12-27 13:48:45 +0700

    Terbaru pokoknamah

--- Tubes_Everyday/routes/web.php
+++ Tubes_Everyday/routes/web.php
@@ -7,7 +7,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::get('/', [AuthController::class, 'showLoginForm'])->name('login');
 Route::get('/signup', [AuthController::class, 'showSignupForm'])->name('signup');
 Route::get('/dashboard', [AuthController::class, 'showHalamanUtama'])->name('dashboard');
 Route::post('/login', [AuthController::class, 'login']);
